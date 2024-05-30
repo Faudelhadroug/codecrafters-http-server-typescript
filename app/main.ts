ec450401faaa233e5c91a7156ed2629a49723d38
Faudel <hadroug.faudel@gmail.com>
2024-05-30 02:20:09 +0200

    Extract URL path #IH0

--- app/main.ts
+++ app/main.ts
@@ -2,8 +2,17 @@ import * as net from "net"
 import { Buffer } from "buffer"
 
 const server = net.createServer((socket: Buffer) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n")
-    socket.end()
+    const OK_200_RESPONSE: string = "HTTP/1.1 200 OK\r\n\r\n";
+    const NotFound_400_RESPONSE: string = "HTTP/1.1 404 Not Found\r\n\r\n"
+    socket.on("data", (data: Buffer) => {
+        const request = data.toString()
+        const path = request.split(" ")[1]
+        const response = path === "/"
+            ? OK_200_RESPONSE
+            : NotFound_400_RESPONSE
+        socket.write(response)
+        socket.end()
+    })
 })
 
 const PORT = 4221
