7c64c8cf7ad9cbcc21f69c64ee431a8495a6dab3
Faudel <hadroug.faudel@gmail.com>
2024-05-30 03:04:53 +0200

    Respond with body #CN2 (2)

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,23 @@
 import * as net from "net"
 import { Buffer } from "buffer"
 
-const OK_200_RESPONSE: string = "HTTP/1.1 200 OK\r\n";
+const OK_200_RESPONSE: string = "HTTP/1.1 200 OK\r\n"
 const NotFound_400_RESPONSE: string = "HTTP/1.1 404 Not Found\r\n\r\n"
-
 const ContentType_TextPlain_RESPONSE = "Content-Type: text/plain\r\n"
 
 const server = net.createServer((socket: Buffer) => {
     socket.on("data", (data: Buffer) => {
         const request = data.toString()
         const path = request.split(" ")[1]
-        const echoRequest = path.split("/echo/")
-        const ContentLength_REPONSE = `Content-Length: ${echoRequest[1].length}\r\n`
-        const response = `${OK_200_RESPONSE}${ContentType_TextPlain_RESPONSE}${ContentLength_REPONSE}\r\n${echoRequest[1]}`
-        socket.write(response)
+        const echoRequest = path.split("/echo/")[1]
+        if(echoRequest){
+            const ContentLength_REPONSE = `Content-Length: ${echoRequest.length}\r\n`
+            const response = `${OK_200_RESPONSE}${ContentType_TextPlain_RESPONSE}${ContentLength_REPONSE}\r\n${echoRequest}`
+            socket.write(response)
+            socket.end()
+            return;
+        }
+        socket.write(NotFound_400_RESPONSE)
         socket.end()
     })
 })
